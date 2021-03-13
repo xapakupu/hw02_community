cce490c1b981bf9c184716374a24aaa08fe1a6c4
Алексей Онгоев <aongoev@yandex.ru>
2021-03-13 18:49:12 +0300

    Спринт 3. Итерация 1

---
+++ posts/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Post, Group
+
+
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk', "text", "pub_date", "author")
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",)
+    # добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",)
+    empty_value_display = "-пусто-"
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'description')
+    search_field = ('title')
+    list_filter = ('title',)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
+# Register your models here.
---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('group/<slug:slug>/', views.group_posts, name='posts'),
+    path("", views.index, name="index"),
+]
